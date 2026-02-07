48137b209cbc2dc80920c46d23896e18de301d41
MinecrackTyler <minecracktyler@gmail.com>
2026-02-07 17:41:20 -0600

    Fix

--- Patches/HUDTurretCrosshairPatch.cs
+++ Patches/HUDTurretCrosshairPatch.cs
@@ -4,34 +4,60 @@ using UnityEngine.UI;
 
 namespace CustomWeapons.Patches
 {
-    [HarmonyPatch(typeof(HUDTurretCrosshair))] //it hates my grappling hook, may not be needed but i am too lazy to check
+    [HarmonyPatch(typeof(HUDTurretCrosshair))]
     public static class HUDTurretCrosshairPatch
     {
+        static readonly AccessTools.FieldRef<HUDTurretCrosshair, Turret> TurretRef =
+            AccessTools.FieldRefAccess<HUDTurretCrosshair, Turret>("turret");
+
+        static readonly AccessTools.FieldRef<HUDTurretCrosshair, Gun> GunRef =
+            AccessTools.FieldRefAccess<HUDTurretCrosshair, Gun>("gun");
+
+        static readonly AccessTools.FieldRef<HUDTurretCrosshair, Image> CrosshairRef =
+            AccessTools.FieldRefAccess<HUDTurretCrosshair, Image>("crosshair");
+
+        static readonly AccessTools.FieldRef<HUDTurretCrosshair, Image> CircleRef =
+            AccessTools.FieldRefAccess<HUDTurretCrosshair, Image>("circle");
+
+        static readonly AccessTools.FieldRef<HUDTurretCrosshair, Image> ReadinessCircleRef =
+            AccessTools.FieldRefAccess<HUDTurretCrosshair, Image>("readinessCircle");
+
         [HarmonyPatch("Refresh")]
         [HarmonyPrefix]
-        public static bool Prefix(HUDTurretCrosshair __instance, Camera mainCamera, out Vector3 crosshairPosition)
+        public static bool RefreshPrefix(
+            HUDTurretCrosshair __instance,
+            Camera mainCamera,
+            out Vector3 crosshairPosition)
         {
+            Debug.Log("[Test] Applying CrosshairPatch");
             crosshairPosition = Vector3.one * 10000f;
 
-            var turret = Traverse.Create(__instance).Field("turret").GetValue<Turret>();
-            var gun = Traverse.Create(__instance).Field("gun").GetValue<Gun>();
-            var crosshair = Traverse.Create(__instance).Field("crosshair").GetValue<Image>();
-            var circle = Traverse.Create(__instance).Field("circle").GetValue<Image>();
-            var readinessCircle = Traverse.Create(__instance).Field("readinessCircle").GetValue<Image>();
+            Turret turret = TurretRef(__instance);
+            if (turret == null)
+                return false;
+
+            Gun gun = GunRef(__instance);
+            Image crosshair = CrosshairRef(__instance);
+            Image circle = CircleRef(__instance);
+            Image readinessCircle = ReadinessCircleRef(__instance);
 
             Vector3 direction = turret.GetDirection();
-            bool flag = turret.IsOnTarget();
+            bool onTarget = turret.IsOnTarget();
 
-            if (Vector3.Dot(mainCamera.transform.forward, direction - mainCamera.transform.position) > 0f)
+            if (Vector3.Dot(mainCamera.transform.forward,
+                            direction - mainCamera.transform.position) > 0f)
             {
-                crosshairPosition = SceneSingleton<CameraStateManager>.i.mainCamera.WorldToScreenPoint(direction);
+                crosshairPosition =
+                    SceneSingleton<CameraStateManager>.i.mainCamera
+                        .WorldToScreenPoint(direction);
+
                 crosshairPosition.z = 0f;
                 __instance.transform.position = crosshairPosition;
                 crosshair.enabled = true;
 
-                float reloadProgress = 0f;
-                if (gun != null)
-                    reloadProgress = gun.GetReloadProgress();
+                float reloadProgress = gun != null
+                    ? gun.GetReloadProgress()
+                    : 0f;
 
                 if (gun != null && reloadProgress > 0f)
                 {
@@ -40,15 +66,19 @@ namespace CustomWeapons.Patches
                         readinessCircle.enabled = true;
                         crosshair.color = Color.red + Color.green * 0.5f;
                     }
+
                     readinessCircle.fillAmount = reloadProgress;
                 }
-                else if (gun != null && readinessCircle.enabled)
+                else
                 {
-                    readinessCircle.enabled = false;
-                    crosshair.color = Color.green;
+                    if (readinessCircle.enabled)
+                    {
+                        readinessCircle.enabled = false;
+                        crosshair.color = Color.green;
+                    }
                 }
-
-                circle.enabled = flag && (gun == null || reloadProgress <= 0f);
+                
+                circle.enabled = onTarget && reloadProgress <= 0f;
             }
             else
             {
@@ -56,7 +86,7 @@ namespace CustomWeapons.Patches
                 readinessCircle.enabled = false;
                 crosshair.enabled = false;
             }
-            
+
             return false;
         }
     }
